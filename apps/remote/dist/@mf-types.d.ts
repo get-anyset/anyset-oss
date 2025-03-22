f059ad330e2eae3317bdbb702be304b96037af29
buffolander <bruno@brdu.net>
2025-03-22 00:47:10 +0000

    Should never commit dist directories

--- apps/remote/dist/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/Button' | 'REMOTE_ALIAS_IDENTIFIER/Header';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/Header' ? typeof import('REMOTE_ALIAS_IDENTIFIER/Header') :T extends 'REMOTE_ALIAS_IDENTIFIER/Button' ? typeof import('REMOTE_ALIAS_IDENTIFIER/Button') :any;
--- apps/remote/dist/@mf-types/Button.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './compiled-types/components/Button/index';
-export { default } from './compiled-types/components/Button/index';
