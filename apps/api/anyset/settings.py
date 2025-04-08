63db78e1eb48bb9a78f8279a7138c332aed16812
buffolander <bruno@brdu.net>
2025-04-08 10:49:41 +0100

    Add support for loading env vars from dotenv files

--- apps/api/anyset/settings.py
+++ apps/api/anyset/settings.py
@@ -2,11 +2,14 @@
 
 from pathlib import Path
 
+from dotenv import load_dotenv
 from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import tomli
 
 from .models import Dataset
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
