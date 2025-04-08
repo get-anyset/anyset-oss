efa74903a8e2465a6d0407add0e4339180c8da9d
buffolander <bruno@brdu.net>
2025-04-08 10:07:15 +0100

    Add log level to application settings

--- apps/api/anyset/settings.py
+++ apps/api/anyset/settings.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-from pydantic import Field, computed_field
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import tomli
 
@@ -26,7 +26,8 @@ class Settings(BaseSettings):
 
     host: str = "0.0.0.0"
     port: int = 8000
-    debug: bool = Field(default=True, description="Enable debug mode")
+    debug: bool = True
+    log_level: str = "INFO"
 
     cors_allow_credentials: bool = True
     cors_allow_headers: list[str] = ["*"]
