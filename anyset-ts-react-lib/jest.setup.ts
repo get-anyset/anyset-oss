14031c07a2991dbe70e2cadd09653f0144074b85
buffolander <bruno@brdu.net>
2025-06-15 17:35:50 +0100

    Fix jest setup

--- anyset-ts-react-lib/jest.setup.ts
+++ anyset-ts-react-lib/jest.setup.ts
@@ -1,7 +1,6 @@
 // Optional: configure or set up a testing framework before each test.
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-
 // Example: Import jest-dom matchers
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 // You can add other global setups here, like mocking global objects, etc.
